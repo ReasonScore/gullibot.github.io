c08d4513f3c60e8077a4f535d08ad5fb862c332a
Bentley Davis <Bentley@BentleyDavis.com>
2022-01-19 14:28:56 -0600

    Updates

--- static/js/ReasonScoreFull2.js
+++ static/js/ReasonScoreFull2.js
@@ -8,8 +8,8 @@
     ]
     const scripts2 = [
         
-        rootAddress + "/static/js/2.42e90fd6.chunk.js",
-        rootAddress + "/static/js/main.c1da7012.chunk.js",
+        rootAddress + "undefined",
+        rootAddress + "1",
     ]
 
     // default settings
@@ -32,7 +32,7 @@
 
     // Add Standard CSS
     const r = document.createElement('div')
-    r.innerHTML = `<link href="${rootAddress}/static/css/main.e76792a4.chunk.css" rel="stylesheet">`;
+    r.innerHTML = `<link href="${rootAddress}1" rel="stylesheet">`;
     document.body.appendChild(r);
 
     // Run any code that requires Javascript to have already been loaded and run 
